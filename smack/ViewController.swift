81c336be03305d918c56cd0fe33ac645ffbb082e
Sergey Neupokoev <Sergey@Sergeys-MacBook-Pro.local>
2019-04-28 23:36:51 +0400

    initializing new project

---
+++ smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smack
+//
+//  Created by Sergey Neupokoev on 28/4/19.
+//  Copyright © 2019 Sergey Neupokoev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
