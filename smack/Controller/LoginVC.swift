303f0607319997015bd12b9ec69c2febc541fe32
Sergey Neupokoev <Sergey@Sergeys-MacBook-Pro.local>
2019-07-27 22:08:24 +0400

    New VC

---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Sergey Neupokoev on 27/7/19.
+//  Copyright © 2019 Sergey Neupokoev. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+
+}
---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Sergey Neupokoev on 27/7/19.
+//  Copyright © 2019 Sergey Neupokoev. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
