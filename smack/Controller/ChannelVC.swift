bc38da660344d52cf6336a00c205029ca9358363
Sergey Neupokoev <Sergey@Sergeys-MacBook-Pro.local>
2019-07-27 20:21:05 +0400

    Customized the view

--- smack/Controller/ChannelVC.swift
+++ smack/Controller/ChannelVC.swift
@@ -12,19 +12,11 @@ class ChannelVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 100
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
