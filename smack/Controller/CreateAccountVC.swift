e6e5fa3324ac49c99594ca59183dbed091cd0b36
Sergey Neupokoev <Sergey@Sergeys-MacBook-Pro.local>
2019-09-27 23:34:57 +0400

    Registring the user and button style changed

--- smack/Controller/CreateAccountVC.swift
+++ smack/Controller/CreateAccountVC.swift
@@ -9,11 +9,39 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
+    
+    
+    //MARK: Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImage: UIImageView!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    //MARK: Buttons actions
+    @IBAction func createAccountPressed(_ sender: UIButton) {
+        guard let email = emailTxt.text , emailTxt.text != "" else {return}
+        guard let password = passwordTxt.text , passwordTxt.text != "" else {return}
+        
+        AuthService.instance.registerUser(email: email, password: password, completion: {(success) in
+            if success {
+                print("registered user!")
+            }
+        })
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: UIButton) {
+    }
+    
+    
     
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
