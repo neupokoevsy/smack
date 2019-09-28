5765608a353ab9490a5646796cc02ff06ec26812
Sergey Neupokoev <Sergey@Sergeys-MacBook-Pro.local>
2019-09-28 17:19:06 +0400

    User SwiftyJSON

--- smack/Services/AuthService.swift
+++ smack/Services/AuthService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class AuthService {
     
@@ -27,7 +28,7 @@ class AuthService {
     
     var authToken: String {
         get{
-            return defaults.value(forKey: TOKEN_KEY) as! String
+            return defaults.value(forKey: TOKEN_KEY) as? String ?? "TOKEN NOT RECEIVED"
         }
         set {
             defaults.set(newValue, forKey: TOKEN_KEY)
@@ -48,9 +49,6 @@ class AuthService {
     func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
         
         let lowerCaseEmail = email.lowercased()
-        let header = [
-            "Content-type": "application/json; charset=utf-8"
-        ]
         
         let body: [String: Any] = [
             "email": lowerCaseEmail,
@@ -59,7 +57,7 @@ class AuthService {
 
         //MARK: Alamofire request
         
-        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString {(response) in
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseString {(response) in
             if response.result.error == nil {
                 completion(true)
             } else {
@@ -69,6 +67,39 @@ class AuthService {
         }
         
     }
+    
+    func loginUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON {(response) in
+            if response.result.error == nil {
+                
+                //MARK: Using SwiftyJSON
+                
+                guard let data = response.data else {
+                    return
+                }
+                
+                let json = JSON(data: data)
+                self.userEmail = json["user"].stringValue
+                self.authToken = json["token"].stringValue
+                
+                self.isLoggedIn = true
+                completion(true)
+                
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+        
+    }
+
     
 }
 
